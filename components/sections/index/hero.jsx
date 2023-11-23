ba257d7aba028590c8d6724f36c34ec67602a0fb
Romi Julianto <romyjulians@gmail.com>
2023-11-23 07:21:24 +0700

    fix: remove file unused

--- components/sections/index/hero.jsx
+++ components/sections/index/hero.jsx
@@ -1,19 +1,13 @@
-import { useState } 		from 'react';
-import { TypeAnimation } 	from 'react-type-animation';
+import { useState } from 'react';
+import { TypeAnimation } from 'react-type-animation';
 
-import Section 		from '../../structure/section';
-import Container 	from '../../structure/container';
-
-import space		from '../../utils/spacing.util';
-
-import Icon 		from '../../utils/icon.util'
-
-import HeroBg		from '../../blocks/hero.bg/bg-color-1';
-
-import hero 		from '../../../styles/sections/index/hero.module.scss';
-import button 		from '../../../styles/blocks/button.module.scss';
-
-import content		from '../../../content/index/hero.json'
+import Section from '../../structure/section';
+import Container from '../../structure/container';
+import space from '../../utils/spacing.util';
+import HeroBg from '../../blocks/hero.bg/bg-color-1';
+import hero from '../../../styles/sections/index/hero.module.scss';
+import button from '../../../styles/blocks/button.module.scss';
+import content from '../../../content/index/hero.json'
 
 
 /**
@@ -38,11 +32,11 @@ export default function Hero() {
 						content.intro.startDelay,
 						() => { setTypingStatus('typing') },
 						content.intro.start,
-						() => {	setTypingStatus('typed') },
+						() => { setTypingStatus('typed') },
 						content.intro.deleteDelay,
-						() => {	setTypingStatus('deleting') },
+						() => { setTypingStatus('deleting') },
 						content.intro.end,
-						() => {	setTypingStatus('deleted') },
+						() => { setTypingStatus('deleted') },
 						content.intro.restartDelay,
 					]}
 					speed={content.intro.speed}
@@ -53,23 +47,23 @@ export default function Hero() {
 				<section>
 					<h1 className={hero.header}>
 						{content.header.name}
-						</h1>
+					</h1>
 					<h1 className={`${hero.header} ${hero.primaryDim}`}>
 						{content.header.usp}
 					</h1>
 				</section>
 				<section>
 					<p className={`${hero.primaryBright} subtitle ${space(["verticalLrg"])}`}>
-						{ content.paragraph }
-					</p>					
+						{content.paragraph}
+					</p>
 				</section>
 				<section>
-					<button	className={`button ${button.primary}`}
-							onClick={ () => window.location = 'mailto:hello@andrewnelson.net' } >
+					<button className={`button ${button.primary}`}
+						onClick={() => window.location = 'mailto:romyjulians@gmail.com'} >
 						{content.buttons.primary.title}
 					</button>
 					<button className={`button ${button.secondary} leaveSite`}
-							onClick={ ()=> window.open("https://www.linkedin.com/in/--andrewnelson/", "_blank") } >
+						onClick={() => window.open("https://www.linkedin.com/in/romijulianto/", "_blank")} >
 						{content.buttons.secondary.title}
 					</button>
 				</section>
--- tests/unit/sections/hero.test.jsx
+++ tests/unit/sections/hero.test.jsx
@@ -8,7 +8,7 @@ describe('Hero section parts', () => {
 		render(<Hero />)
 		
 		const heading = screen.getByRole('heading', {
-			name: /Andrew Nelson\./i,
+			name: /Romi Julianto\./i,
 		})
 		
 		expect(heading).toBeInTheDocument()
